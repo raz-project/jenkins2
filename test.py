d815a71f205b63d49494d28576ddcd8b9b996bb7
raz-project <srazi88@walla.co.il>
2025-02-04 22:18:52 +0200

    modi again

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ number1 = 25
 number2 = 22
 while poli:
  
- if number1.isdigit() and number2.isdigit():  # Ensures the input is a valid number
+   # Ensures the input is a valid number
     if number1 == number1[::-1] or number2 == number2[::-1]:  # Compare original with its reverse
         print(f"{number1} is a palindrome.")
         print(f"{number1} is a palindrome.")
@@ -13,5 +13,3 @@ while poli:
     else:
         print(f"{number1} is not a palindrome.")
         print(f"{number1} is not a palindrome.")
- else:
-     print("Please enter a valid number.")
