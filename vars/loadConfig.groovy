95105220c917c54486d33a8f365092da95f11ac1
raz-project <srazi88@walla.co.il>
2025-02-04 15:07:56 +0200

    trying to update

---
+++ vars/loadConfig.groovy
@@ -0,0 +1,9 @@
+groovy
+CopyEdit
+import groovy.json.JsonSlurper
+
+def call(String fileName = 'config.json') {
+    def fileContent = libraryResource(fileName)
+    return new JsonSlurper().parseText(fileContent)
+}
+
